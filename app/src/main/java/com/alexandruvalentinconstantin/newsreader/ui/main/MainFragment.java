4fbcb0d492022767ce06a862f4c83928c3400ac8
Alexandru-Valentin Constantin <constantin.valentin0102@gmail.com>
2021-11-07 15:11:01 +0200

    Added data view binding

--- app/src/main/java/com/alexandruvalentinconstantin/newsreader/ui/main/MainFragment.java
+++ app/src/main/java/com/alexandruvalentinconstantin/newsreader/ui/main/MainFragment.java
@@ -1,5 +1,4 @@
 package com.alexandruvalentinconstantin.newsreader.ui.main;
-
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -8,11 +7,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.alexandruvalentinconstantin.newsreader.R;
+import com.alexandruvalentinconstantin.newsreader.databinding.MainFragmentBinding;
+
 
 public class MainFragment extends Fragment {
 
@@ -22,18 +23,23 @@ public class MainFragment extends Fragment {
         return new MainFragment();
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        Log.d(this.getClass().getSimpleName(), "onCreate: " + mViewModel);
+
+    }
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_fragment, container, false);
-    }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
-        // TODO: Use the ViewModel
-    }
+        MainFragmentBinding binding = MainFragmentBinding.inflate(inflater, container, false);
+        binding.setViewModel(mViewModel);
+        return binding.getRoot();
 
+    }
 }
